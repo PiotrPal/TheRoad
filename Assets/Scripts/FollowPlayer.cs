d21fc84d2cf8ecf547746dc36c940237478a330a
PiotrPal <133272105+PiotrPal@users.noreply.github.com>
2024-04-24 01:53:00 +0200

    skrypts for player and camera

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+    public GameObject player;
+    private Vector3 startPosition;
+    // Start is called before the first frame update
+    void Start() {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate() {
+        transform.position = player.transform.position + startPosition;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,13 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    // Start is called before the first frame update
+    public float speed = 12.0f;
+
     void Start() {
 
     }
 
-    // Update is called once per frame
     void Update() {
-        //move code
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
     }
 }
+
+
+
+
+//public class PlayerController : MonoBehaviour {
+//    //Rigidbody m_Rigidbody;
+//    //public int speed = 5;
+//    //public float m_Speed = 5f;
+//    // Start is called before the first frame update
+//    void Start() {
+//        //m_Rigidbody = GetComponent<Rigidbody>();
+//    }
+
+//    // Update is called once per frame
+//    void Update() {
+//        // 60 razy na 1s 
+//        //move code
+//        //transform.Translate(0, 0, 1);
+//        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+
+//        //Vector3 m_Input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+//        //float horizontal = Input.GetAxis("Horizontal");
+//        //float vertical = Input.GetAxis("Vertical");
+
+//        //Vector3 move = new Vector3(horizontal, 0, vertical);
+//        //m_Rigidbody.MovePosition(transform.position + move  * Time.deltaTime * speed);
+//    }
+//}
